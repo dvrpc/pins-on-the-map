82e5492eed71b49f2f8bf1c56cbcd52feef3c259
aaronfraint <aaronfraint@icloud.com>
2021-09-13 12:47:10 -0400

    set pins index to landing page

--- pins_on_the_map/urls.py
+++ pins_on_the_map/urls.py
@@ -16,4 +16,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("pins/", include("pins.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("pins.urls"))]
