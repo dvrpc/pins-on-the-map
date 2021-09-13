0b2d912cbdd094edfc825fb55a985dd0ce37de74
aaronfraint <aaronfraint@icloud.com>
2021-09-13 09:21:09 -0400

    update ALLOWED_HOSTS

--- pins_on_the_map/settings.py
+++ pins_on_the_map/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "pins-on-the-map.dev.dvrpc.org'"]
 
 
 # Application definition
