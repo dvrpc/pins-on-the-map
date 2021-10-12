b21a2abea1ae8c25d1699761c94a079d25349f0e
aaronfraint <aaronfraint@icloud.com>
2021-10-12 16:57:08 -0400

    import new stuff

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,16 @@
 import "mapbox-gl/dist/mapbox-gl.css";
-import "./map_style.css";
+import "./css/map_style.css";
+import "./css/alerts.css";
 
 import { map } from "./js/map";
 import { wire_click_logic } from "./js/click.js";
 import { hover_setup } from "./js/hover.js";
 import { setup_button_listeners } from "./js/click_main_buttons.js";
-import { load_pins_from_api } from "./js/load_data";
-import { setup_minor_button_listeners } from "./js/click_minor_buttons"
+import {
+  load_pins_from_api,
+  add_tag_options_to_survey_form,
+} from "./js/load_data";
+import { setup_minor_button_listeners } from "./js/click_minor_buttons";
 
 const CLUSTER_LEVEL = 15;
 
@@ -19,4 +23,6 @@ map.on("load", function () {
 
   setup_button_listeners();
   setup_minor_button_listeners();
+
+  add_tag_options_to_survey_form(map);
 });
