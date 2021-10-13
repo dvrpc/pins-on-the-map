76a01c3e70e447c115f8a0faeb230b9e5a59219d
aaronfraint <aaronfraint@icloud.com>
2021-10-13 12:32:58 -0400

    load study area data

--- assets/index.js
+++ assets/index.js
@@ -9,12 +9,14 @@ import { setup_button_listeners } from "./js/click_main_buttons.js";
 import {
   load_pins_from_api,
   add_tag_options_to_survey_form,
+  load_study_area_from_geojson,
 } from "./js/load_data";
 
 const CLUSTER_LEVEL = 15;
 
 map.on("load", function () {
   load_pins_from_api(map);
+  load_study_area_from_geojson(map);
 
   // Set up the click event & hover
   wire_click_logic(map);
