0f9b63613372a6d04f5593be52b57e45a97b67c6
aaronfraint <aaronfraint@icloud.com>
2021-10-15 14:51:29 -0400

    add filter helper

--- assets/js/switches.js
+++ assets/js/switches.js
@@ -24,10 +24,15 @@ const set_mouse_to_normal = (map) => {
   map.getCanvas().style.cursor = "";
 };
 
+const select_pin_by_id = (map, pin_id) => {
+  map.setFilter("selected-pin", ["==", "pin_id", pin_id]);
+};
+
 export {
   set_display_to_id,
   user_wants_to_add_pin,
   toggle_button_state,
   set_mouse_to_crosshair,
   set_mouse_to_normal,
+  select_pin_by_id,
 };
