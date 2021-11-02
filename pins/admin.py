4b7ebbd596dceb7e531458502f1a0cfe852746fa
aaronfraint <aaronfraint@icloud.com>
2021-11-02 16:18:11 -0400

    add survey model

--- pins/admin.py
+++ pins/admin.py
@@ -1,8 +1,8 @@
 from django.contrib.gis import admin
 
-from .models import Pin, Topic, Comment, MapUser
+from .models import Pin, Topic, Comment, MapUser, LongformSurvey
 
 admin.site.register(Pin, admin.GeoModelAdmin)
 
-for klass in [Topic, Comment, MapUser]:
+for klass in [Topic, Comment, MapUser, LongformSurvey]:
     admin.site.register(klass)
